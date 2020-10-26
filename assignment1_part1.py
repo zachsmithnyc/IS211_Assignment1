99d31bfc3a165a25cbd974538a8725377b74c694
cuban.quant <cuban.quant@gmail.com>
2020-10-25 22:15:25 -0400

    Update and rename assignment1.py to assignment1_part1.py

---
+++ assignment1_part1.py
@@ -0,0 +1,19 @@
+
+def listDivide(numbers, divide):
+    """
+    The function returns the number of elements in the numbers list that are divisibleby divide
+    """
+    pass
+
+def testListDivide():
+    """
+    Test listDivide
+    """
+    assert listDivide([1,2,3,4,5]) == 2
+    assert listDivide([2,4,6,8,10]) == 5
+    assert listDivide([30, 54, 63,98, 100], divide=10) == 2
+    assert listDivide([]) == 0
+    assert listDivide([1,2,3,4,5], 1) == 5
+    
+if __name__ == "__main__":
+    testListDivide()
