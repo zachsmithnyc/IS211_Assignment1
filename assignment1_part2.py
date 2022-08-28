0d5b4a6aafd168fab918594f0401b0902f05338a
Zachary Smith <zachary.smith69@spsmail.cuny.edu>
2022-08-28 13:28:39 -0400

    Completed Assignment 1 Part 2

--- assignment1_part2.py
+++ assignment1_part2.py
@@ -1,11 +1,16 @@
 
 class Book:
     def __init__(self, author, title):
-        pass
+        self.author = author
+        self.title = title
     
     def display(self):
-        pass
+        print(f"{self.title}, written by {self.author}.")
 
 
 if __name__ == "__main__":
-    pass
+    b1 = Book("J. K. Rowling", "Harry Potter and The Goblet of Fire")
+    b2 = Book("Walter Scott", "Ivanhoe: A Romance")
+
+    b1.display()
+    b2.display()
